e7b0e0514bcc1267bce484ede0be0a1204224cef
katemurraay <117349221@umail.ucc.ie>
2022-03-31 12:52:11 +0100

    Initial Commit

---
+++ Players_Core_Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Players_Core_Data
+//
+//  Created by Kate Murray on 31/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
